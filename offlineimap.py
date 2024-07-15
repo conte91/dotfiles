b9e871f8672ef8ed6b5f4032f0e53a489fb5d8fd
Simone Baratta <Conte91@gmail.com>
2024-07-15 22:46:50 +0200

    Use python2 for offlineimap stuff

--- offlineimap.py
+++ offlineimap.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 import os
 import subprocess
 import sys
@@ -7,7 +9,7 @@ def _read_pass(account_name, pass_name):
     cmd = ['pass', actual_pass_name]
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=sys.stderr)
     out, _ = p.communicate()
-    return out
+    return out.decode('utf-8')
 
 def get_client_id(account_name):
     return _read_pass(account_name, 'oauth2_client_id')
@@ -17,3 +19,15 @@ def get_client_secret(account_name):
 
 def get_refresh_token(account_name):
     return _read_pass(account_name, 'oauth2_refresh_token')
+
+def get_app_password(account_name):
+    return _read_pass(account_name, 'app_password')
+
+def get_web_password(account_name):
+    cmd = ['pass', account_name]
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=sys.stderr)
+    out, _ = p.communicate()
+    # Web passwords have a password and a username on separate lines.
+    # Only keep the password.
+    out = out.split(b'\n')[0]
+    return out.decode('utf-8')
